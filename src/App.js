538663649b43606a2e8320ec0b9150f182830c84
teddyrugged <jakpaetuk@gmail.com>
2023-04-21 21:02:29 +0100

    Home page and signin page added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Offers from "./pages/Offers.jsx";
 import Profile from "./pages/Profile.jsx";
-import SignIn from "./pages/SignIn.jsx";
+import SignIn from "./pages/Signin.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgetpassword" element={<ForgotPassword />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forget-password" element={<ForgotPassword />} />
         </Routes>
       </Router>
     </>
